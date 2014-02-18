3a4d699d87475994afc5e4afd6d6f4750ebc803c
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2014-02-17 23:51:54 -0800

    Add grammar rules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def index
 
   end
+
+  def grammar
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SpokenCode::Application.routes.draw do
   root 'application#index'
 
+  get '/grammar', to: 'application#grammar'
+
   devise_for :users
 end
